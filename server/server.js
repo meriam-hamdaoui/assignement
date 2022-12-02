032be2eb71bbf918868e532a8add06bb8d6253c6
meriam <hamdaoui.meriam85@gmail.com>
2022-12-02 11:38:26 +0100

    get by id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const jsonServer = require("json-server");
 const jwt = require("jsonwebtoken");
+const { data } = require("jquery");
 
 const server = jsonServer.create();
 const userdb = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
@@ -13,39 +14,27 @@ server.use(jsonServer.defaults());
 const SECRET_KEY = "72676376";
 const expiresIn = "1h";
 
+// create token
 const createToken = (payload) => {
   return jwt.sign(payload, SECRET_KEY, { expiresIn });
 };
 
+// get all users
 server.get("/api/users", (req, res) => {
-  return res.status(200).json(userdb.users);
+  fs.readFile("./db.json", (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = error;
+      res.status(status).json({ status, message });
+      return;
+    }
+    data = JSON.parse(data.toString());
+
+    res.status(200).json(data.users);
+  });
 });
 
-const isLoginAuthenticated = ({ email, password }) => {
-  return (
-    userdb.users.findIndex(
-      (user) => user.email === email && user.password === password
-    ) !== -1
-  );
-};
-
-server.post("/api/auth/login", (req, res) => {
-  const { email, password } = req.body;
-
-  if (!isLoginAuthenticated({ email, password })) {
-    const status = 401;
-    const message = "Incorrect Email or Password";
-    res.status(status).json({ status, message });
-    return;
-  }
-  const user = userdb.users.filter(
-    (user) => user.email === email && user.password === password
-  );
-  const access_token = createToken({ email, password });
-
-  res.status(200).json({ user: user, token: access_token });
-});
-
+// sign up
 const isRegisterAuthenticated = ({ email }) => {
   return userdb.users.findIndex((user) => user.email === email) !== -1;
 };
@@ -85,12 +74,40 @@ server.post("/api/auth/register", (req, res) => {
         return;
       }
     });
+    res.status(200).json({ newUser: req.body, users: data.users });
   });
 
   // const access_token = createToken({ email, password });
-  res.status(200).json({ user: req.body });
 });
 
+// signin
+const isLoginAuthenticated = ({ email, password }) => {
+  return (
+    userdb.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
+};
+
+//
+server.post("/api/auth/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!isLoginAuthenticated({ email, password })) {
+    const status = 401;
+    const message = "Incorrect Email or Password";
+    res.status(status).json({ status, message });
+    return;
+  }
+  const user = userdb.users.filter(
+    (user) => user.email === email && user.password === password
+  );
+  const access_token = createToken({ email, password });
+
+  res.status(200).json({ user: user, token: access_token });
+});
+
+// delete a user
 server.delete("/api/auth/delete/:id", (req, res) => {
   const { id } = req.params;
 
@@ -116,6 +133,27 @@ server.delete("/api/auth/delete/:id", (req, res) => {
       }
       res.status(200).json({ data: data.users });
     });
+  });
+});
+
+// get user by id
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  fs.readFile("./db.json", (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = error;
+      res.status(status).json({ status, message });
+      return;
+    }
+    data = JSON.parse(data.toString());
+
+    const userIndex = data.users.findIndex(
+      (user) => Number(user.id) === Number(id)
+    );
+
+    res.status(200).json(data.users[userIndex]);
   });
 });
 
